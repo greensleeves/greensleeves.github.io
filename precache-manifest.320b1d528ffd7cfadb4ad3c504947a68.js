8cd562176f9646b4ed334f6ba151420d7334ada7
greensleeves <mytower@yandex.ru>
2020-09-25 09:36:46 +0300

    Add files via upload

---
+++ precache-manifest.320b1d528ffd7cfadb4ad3c504947a68.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aba3bb94eb38139d08d38f6e2cf6342",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b290b984b702c71d413",
+    "url": "/static/css/2.e4a047e2.chunk.css"
+  },
+  {
+    "revision": "20810bd82cbc73fe99b5",
+    "url": "/static/css/main.c215f828.chunk.css"
+  },
+  {
+    "revision": "3b290b984b702c71d413",
+    "url": "/static/js/2.c8403b49.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c8403b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20810bd82cbc73fe99b5",
+    "url": "/static/js/main.e041f61b.chunk.js"
+  },
+  {
+    "revision": "0d2f18cee0646946960b",
+    "url": "/static/js/runtime-main.d69e5064.js"
+  },
+  {
+    "revision": "c149fc92accc81ce7379fe09db15406d",
+    "url": "/static/media/Svydkoy_preview.c149fc92.mp4"
+  },
+  {
+    "revision": "818be68f11373b1524c8c2a27cd9a192",
+    "url": "/static/media/background_gray.818be68f.jpg"
+  },
+  {
+    "revision": "e41dd868ea2b0218d201128f38086be6",
+    "url": "/static/media/background_rose.e41dd868.jpg"
+  },
+  {
+    "revision": "0911497295663d9cf9a9986c23f6c1bc",
+    "url": "/static/media/background_yellow.09114972.jpg"
+  },
+  {
+    "revision": "dfff8e4e2124ebec786072e0bad3ecdd",
+    "url": "/static/media/corner_fill.dfff8e4e.svg"
+  },
+  {
+    "revision": "4198bac2b39d4a5c711b384b3b2acd76",
+    "url": "/static/media/corner_opacity.4198bac2.svg"
+  },
+  {
+    "revision": "ea1f91740aedfb7225b52bf1f6ef0d9a",
+    "url": "/static/media/floor_round.ea1f9174.svg"
+  },
+  {
+    "revision": "a220834bc50974c7de72ad75a1ddb742",
+    "url": "/static/media/hotel.a220834b.png"
+  },
+  {
+    "revision": "2d6b7239758118851c4712a953f4cc33",
+    "url": "/static/media/man.2d6b7239.png"
+  },
+  {
+    "revision": "b5acc794e73ebeb274032146581c260b",
+    "url": "/static/media/rhombus.b5acc794.svg"
+  },
+  {
+    "revision": "307767f21d6e0425eb8db9031bf65cd8",
+    "url": "/static/media/ribbon_hotel.307767f2.svg"
+  },
+  {
+    "revision": "5113a54378db4437528905012758d9f1",
+    "url": "/static/media/table.5113a543.png"
+  },
+  {
+    "revision": "16865685b22496a525527778e6ad49b5",
+    "url": "/static/media/window.16865685.png"
+  }
+]);
