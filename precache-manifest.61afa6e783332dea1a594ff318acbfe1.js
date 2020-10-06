5f26e719866a854b0bc96f275e2e77e96527e841
Anton Kondratiev <akondratiev@rg.ru>
2020-10-06 14:53:57 +0300

    build

---
+++ precache-manifest.61afa6e783332dea1a594ff318acbfe1.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d4a5b5dd0f502b4e8b075f6663103c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9389f49765cedceb7f6",
+    "url": "/static/css/2.e4a047e2.chunk.css"
+  },
+  {
+    "revision": "9314fb860668dd62f18a",
+    "url": "/static/css/main.8dcdbeae.chunk.css"
+  },
+  {
+    "revision": "a9389f49765cedceb7f6",
+    "url": "/static/js/2.c415443e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c415443e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9314fb860668dd62f18a",
+    "url": "/static/js/main.322e2ea4.chunk.js"
+  },
+  {
+    "revision": "0d2f18cee0646946960b",
+    "url": "/static/js/runtime-main.d69e5064.js"
+  },
+  {
+    "revision": "5d05a966c603c9fddb498ce6063c17e6",
+    "url": "/static/media/Fetisov_preview.5d05a966.mp4"
+  },
+  {
+    "revision": "1e5f7c260081b491e5ca17d34a0d0b07",
+    "url": "/static/media/Krasheninnikov_preview.1e5f7c26.mp4"
+  },
+  {
+    "revision": "c149fc92accc81ce7379fe09db15406d",
+    "url": "/static/media/Svydkoy_preview.c149fc92.mp4"
+  },
+  {
+    "revision": "cfa1b327114eebc14932e0ca00b6a4c3",
+    "url": "/static/media/Tolstoy_preview.cfa1b327.mp4"
+  },
+  {
+    "revision": "36d55165a9f796353c8b4a47b9ed99c6",
+    "url": "/static/media/background_gray.36d55165.jpg"
+  },
+  {
+    "revision": "d18f4dc282bbbfa66e7b8d1cbbea48ae",
+    "url": "/static/media/background_rose.d18f4dc2.jpg"
+  },
+  {
+    "revision": "290dc79ef2d9be3b060b877a2f09a318",
+    "url": "/static/media/background_yellow.290dc79e.jpg"
+  },
+  {
+    "revision": "ff6827d50be4a4894f4061c838d6acdf",
+    "url": "/static/media/brick_l.ff6827d5.png"
+  },
+  {
+    "revision": "3b519884cac69a98fe3a5e9f0495d0ea",
+    "url": "/static/media/brick_r.3b519884.png"
+  },
+  {
+    "revision": "2a1b6bded45249ab014b82f67b3bf54d",
+    "url": "/static/media/close.2a1b6bde.svg"
+  },
+  {
+    "revision": "dfff8e4e2124ebec786072e0bad3ecdd",
+    "url": "/static/media/corner_fill.dfff8e4e.svg"
+  },
+  {
+    "revision": "4198bac2b39d4a5c711b384b3b2acd76",
+    "url": "/static/media/corner_opacity.4198bac2.svg"
+  },
+  {
+    "revision": "ea1f91740aedfb7225b52bf1f6ef0d9a",
+    "url": "/static/media/floor_round.ea1f9174.svg"
+  },
+  {
+    "revision": "b6bde843a1cd901b3b082c8f8dfdb7e8",
+    "url": "/static/media/hotel.b6bde843.png"
+  },
+  {
+    "revision": "dc91504bd2742431035907ccf6a6e5c5",
+    "url": "/static/media/man.dc91504b.png"
+  },
+  {
+    "revision": "0ac25fb9f50d91b752779bb96e91a0e2",
+    "url": "/static/media/pg.0ac25fb9.png"
+  },
+  {
+    "revision": "b5acc794e73ebeb274032146581c260b",
+    "url": "/static/media/rhombus.b5acc794.svg"
+  },
+  {
+    "revision": "307767f21d6e0425eb8db9031bf65cd8",
+    "url": "/static/media/ribbon_hotel.307767f2.svg"
+  },
+  {
+    "revision": "5113a54378db4437528905012758d9f1",
+    "url": "/static/media/table.5113a543.png"
+  },
+  {
+    "revision": "16865685b22496a525527778e6ad49b5",
+    "url": "/static/media/window.16865685.png"
+  }
+]);
